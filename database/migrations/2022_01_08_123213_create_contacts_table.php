a441c0d14eaa0653a7550fd6ed979937666c6159
Ricardo Soares <rstriquer@gmail.com>
2022-01-08 10:43:13 -0300

    Add contacts table migration

---
+++ database/migrations/2022_01_08_123213_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index()->comment('Contact name. Should be a string of any size greater than 5');
+            $table->unsignedInteger('contact')->comment('Contact number. 9 digits max.');
+            $table->string('email', 191)->unique()->comment('Contact email address. Should be a valid email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
