f8e145686c1859b0ac15007c87fd31cc2596c604
Ricardo Soares <rstriquer@gmail.com>
2022-01-08 16:24:17 -0300

    Adjust home route name

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', PublicContactIndexController::class)->name('public.contact.index');
+Route::get('/', PublicContactIndexController::class)->name('public.index');
